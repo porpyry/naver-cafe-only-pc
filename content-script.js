5a101fa106df839ce766e486176f7cf81eb47a7e
porpyry <porpyry@gmail.com>
2024-12-31 20:40:47 +0900

    v1.2.1

--- content-script.js
+++ content-script.js
@@ -62,10 +62,15 @@ function observeMainDocument(callback) {
 }
 
 function initCafe(doc) {
-    if (options.MTP_article || options.CTA_article) {
+    if (options.MTP_article || options.CTA_article || options.CTA_board) {
         const app = doc.querySelector("#app");
         if (app) {
-            findNext(app, "Article", initArticle);
+            if (options.MTP_article || options.CTA_article) {
+                findNext(app, "Article", initArticle);
+            }
+            if (options.CTA_board) {
+                findNext(app, "MemberProfile", initMemberProfile);
+            }
             return;
         }
     }
@@ -82,31 +87,50 @@ function initCafeMobile() {
     redirectToPC();
 }
 
+async function initMemberProfile(memberProfile) {
+    const articleBoard = await findNext(memberProfile, "article-board");
+    findNextTag(articleBoard, "TABLE", (table) => {
+        for (const item of table.querySelectorAll(".inner_list")) {
+            for (const anchor of [
+                item.querySelector("a.article"), // title
+                item.querySelector("a.cmt") // comment
+            ]) {
+                if (anchor) {
+                    anchor.href = cta_link(anchor.href);
+                }
+            }
+        }
+    });
+}
+
 function initArticleList(mainArea) {
     for (const item of mainArea.querySelectorAll(".article-board .inner_list")) {
         for (const anchor of [
-            item.querySelector("a.article"),
+            item.querySelector("a.article"), // title
             item.querySelector("a.cmt") // comment
         ]) {
-            if (anchor) anchor.href = cta_link(anchor.href);
+            if (anchor) {
+                anchor.href = cta_link(anchor.href);
+            }
         }
     }
     for (const item of mainArea.querySelectorAll("ul.article-album-sub li")) {
         for (const anchor of [
-            item.querySelector("a.album-img"),
+            item.querySelector("a.album-img"), // image
             item.querySelector("a.tit"), // title
             item.querySelector("a.m-tcol-p") // comment
         ]) {
-            if (anchor) anchor.href = cta_link(anchor.href);
+            if (anchor) {
+                anchor.href = cta_link(anchor.href);
+            }
         }
     }
     for (const item of mainArea.querySelectorAll("ul.album-box li")) {
         let href = "";
         for (const anchor of [
-            item.querySelector(".photo a"),
+            item.querySelector(".photo a"), // image
             item.querySelector(".tit a.m-tcol-c") // title
         ]) {
-            console.log(anchor);
             if (anchor) {
                 anchor.href = cta_link(anchor.href) + "&boardtype=I";
                 href = anchor.href;
@@ -117,14 +141,25 @@ function initArticleList(mainArea) {
             a_comment.href = href + "&commentFocus=true";
         }
     }
-
-    function cta_link(href) {
-        const url = new URL("https://cafe.naver.com" + href);
-        const cafeId = url.searchParams.get("clubid");
-        const articleId = url.searchParams.get("articleid");
-        if (cafeId && articleId) {
-            return `https://cafe.naver.com/ca-fe/cafes/${cafeId}/articles/${articleId}${url.search}`;
+    for (const item of mainArea.querySelectorAll("ul.article-movie-sub .card_area")) {
+        for (const anchor of [
+            item.querySelector("a.tit"), // title
+            item.querySelector("a.txt"), // content
+            item.querySelector(".movie-img a") // image
+        ]) {
+            if (anchor) {
+                anchor.href = cta_link(anchor.href);
+            }
         }
+    }
+}
+
+function cta_link(href) {
+    const url = new URL("https://cafe.naver.com" + href);
+    const cafeId = url.searchParams.get("clubid");
+    const articleId = url.searchParams.get("articleid");
+    if (cafeId && articleId) {
+        return `https://cafe.naver.com/ca-fe/cafes/${cafeId}/articles/${articleId}${url.search}`;
     }
 }
 
@@ -141,7 +176,7 @@ async function initArticle(article) {
 
     // change cafe link to article link (cta)
     if (options.CTA_article) {
-        await cta_links(links, oglinks);
+        await cta_links_oglinks(links, oglinks);
     }
 
     function remove_m_fromLinks(links) {
@@ -168,7 +203,7 @@ async function initArticle(article) {
         return text.replace("m.cafe.naver.com", "cafe.naver.com");
     }
 
-    async function cta_links(links, oglinks) {
+    async function cta_links_oglinks(links, oglinks) {
         for (const link of links) {
             const href = await cta_getArticleHref(link.href);
             if (href) {
@@ -340,6 +375,46 @@ function findNext(parent, className, callback) {
                 for (const mutation of mutations) {
                     for (const node of mutation.addedNodes) {
                         if (node.classList?.contains(className)) {
+                            if (findChild()) {
+                                if (!callback) {
+                                    observer.disconnect();
+                                }
+                                return;
+                            }
+                        }
+                    }
+                }
+            }).observe(parent, { childList: true });
+        }
+    };
+}
+
+// tagName: all capital
+function findNextTag(parent, tagName, callback) {
+    if (callback) {
+        return run(callback);
+    }
+    return new Promise((resolve) => run(resolve));
+
+    function run(callbackOrResolve) {
+        const findChild = () => {
+            let foundChild;
+            for (const child of parent.children) {
+                if (child.tagName === tagName) {
+                    foundChild = child;
+                    break;
+                }
+            }
+            if (foundChild) {
+                callbackOrResolve(foundChild);
+            }
+            return foundChild;
+        };
+        if (!findChild() || callback) {
+            new MutationObserver((mutations, observer) => {
+                for (const mutation of mutations) {
+                    for (const node of mutation.addedNodes) {
+                        if (node.tagName === tagName) {
                             if (findChild()) {
                                 if (!callback) {
                                     observer.disconnect();
